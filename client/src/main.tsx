7cadcf03f31c9c31b87cadd9699dfd39677ee879
Ashis Wadhwani <ashiswadhwani05@gmail.com>
2025-12-29 16:14:01 +0530

    deployment changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import {StrictMode} from "react";
 import {BrowserRouter} from "react-router-dom";
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
--- server/server.ts
+++ server/server.ts
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: [
+            "http://localhost:5173",
+            process.env.FRONTEND_URL || "*" // Replace '*' with your Vercel URL later
+        ],
         allowedHeaders: ["Content-Type", "Authorization", "token"]
     }
 ));
